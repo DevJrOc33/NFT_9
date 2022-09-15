7166ca2524491834edf677da9c24a4f5112923b1
Jesse Rios <jrios46@csu.fullerton.edu>
2022-09-14 20:19:27 -0700

    Add project files.

---
+++ NFT_9/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace NFT_9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(94, 340);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ultra Execute 9000 gigaWatts";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 13);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(366, 305);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(82, 385);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "WARNING: MAY USE A LOT OF DATA!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(394, 442);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "StratonAutomations";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ NFT_9/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+
+/*
+
+//D̸̝͔͓̐̽̓è̴͇̟̼̓͋v̸̢͎̦̾̈́J̴̡͉̈́͆͑͜r̴͖͎͔̈́͒͝O̴̘͓̻̔͌͛C̵͚͕͖͑͋͠3̴͖͖̦̽͑͑3̸̘̻͙̾̾̓
+╔══╗─────╔╗─╔═╦═╦═══╦═══╗
+╚╗╗╠═╦═╦═╣╠╦╣║║╔╣╔═╗║╔═╗║
+╔╩╝║╩╬╗║╔╣║╔╣║║╚╬╝╔╝╠╝╔╝║
+╚══╩═╝╚═╩═╩╝╚═╩═╬╗╚╗╠╗╚╗║
+────────────────║╚═╝║╚═╝║
+//D̸̝͔͓̐̽̓è̴͇̟̼̓͋v̸̢͎̦̾̈́J̴̡͉̈́͆͑͜r̴͖͎͔̈́͒͝O̴̘͓̻̔͌͛C̵͚͕͖͑͋͠3̴͖͖̦̽͑͑3̸̘̻͙̾̾̓
+
+*/
+
+
+namespace NFT_9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //main to make image full
+            //export tranparent .png from photoshop 
+            //500 500 
+            Bitmap bitmap = new Bitmap(670, 670);
+
+
+            //layer_one
+            Bitmap layer_one1 = new Bitmap("C:\\Users\\Anon\\Desktop\\nft_algo\\B_1.png");
+            Bitmap layer_one2 = new Bitmap("C:\\Users\\Anon\\Desktop\\nft_algo\\B_2.png");
+            Bitmap layer_one3 = new Bitmap("C:\\Users\\Anon\\Desktop\\nft_algo\\B_3.png");
+            Bitmap layer_one4 = new Bitmap("C:\\Users\\Anon\\Desktop\\nft_algo\\B_4.png");
+
+            //layer_two
+            Bitmap layer_two1 = new Bitmap("C:\\Users\\Anon\\Desktop\\nft_algo\\L_1.png");
+            Bitmap layer_two2 = new Bitmap("C:\\Users\\Anon\\Desktop\\nft_algo\\L_2.png");
+            Bitmap layer_two3 = new Bitmap("C:\\Users\\Anon\\Desktop\\nft_algo\\L_3.png");
+            Bitmap layer_two4 = new Bitmap("C:\\Users\\Anon\\Desktop\\nft_algo\\L_4.png");
+
+            //layer_three
+            Bitmap layer_three1 = new Bitmap("C:\\Users\\Anon\\Desktop\\nft_algo\\F_1.png");
+            Bitmap layer_three2 = new Bitmap("C:\\Users\\Anon\\Desktop\\nft_algo\\F_2.png");
+            Bitmap layer_three3 = new Bitmap("C:\\Users\\Anon\\Desktop\\nft_algo\\F_3.png");
+            Bitmap layer_three4 = new Bitmap("C:\\Users\\Anon\\Desktop\\nft_algo\\F_4.png");
+
+
+
+
+            //while loop
+            int i =0;
+
+
+            //set number of images to create
+            while (i < 50)
+            {
+
+
+                //to introduce propbabilities just add more number of trates so that when 
+                //randomly selected they are fractioned
+
+
+
+                //layer one 
+                /////////////////////////////////////////////////
+                var random1 = new Random();
+                var list1 = new List<Bitmap> { 
+                    layer_one1,
+                    layer_one2,
+                    layer_one3,
+                    layer_one4
+                };
+                int index1 = random1.Next(list1.Count);
+                //secret merger sauce
+                Image merged1 = new Bitmap(bitmap.Width, bitmap.Height);
+                using (Graphics gr1 = Graphics.FromImage(merged1))
+                {
+                    gr1.DrawImage(list1[index1], new Point(0, 0));
+                }
+
+
+
+                //layer_two
+                /////////////////////////////////////////////////
+                var random2 = new Random();
+                var list2 = new List<Bitmap> { 
+                    layer_two1,
+                    layer_two2,
+                    layer_two3,
+                    layer_two4
+                };
+                int index2 = random2.Next(list2.Count);
+                //secret merger sauce
+                Image merged2 = new Bitmap(bitmap.Width, bitmap.Height);
+                using (Graphics gr2 = Graphics.FromImage(merged2))
+                {
+                    gr2.DrawImage(list2[index2], new Point(0, 0));
+                }
+
+
+
+                //layer_three
+                /////////////////////////////////////////////////
+                var random3 = new Random();
+                var list3 = new List<Bitmap> { 
+                    layer_three1,
+                    layer_three2,
+                    layer_three3,
+                    layer_three4
+                };
+                int index3 = random3.Next(list3.Count);
+                //secret merger sauce
+                Image merged3 = new Bitmap(bitmap.Width, bitmap.Height);
+                using (Graphics gr3 = Graphics.FromImage(merged3))
+                {
+                    gr3.DrawImage(list3[index3], new Point(0, 0));
+                }
+
+
+
+
+
+                //output
+                Image output = new Bitmap(bitmap.Width, bitmap.Height);
+                using (Graphics grf = Graphics.FromImage(output))
+                {
+                    grf.DrawImage(merged1, new Point(0, 0));
+                    grf.DrawImage(merged2, new Point(0, 0));
+                    grf.DrawImage(merged3, new Point(0, 0));
+
+
+                    merged1.Dispose();
+                    merged2.Dispose();
+                    merged3.Dispose();
+
+
+                }
+
+                //Display Button
+                pictureBox1.Image = output;
+
+                //Export Button
+                output.Save("C:\\Users\\Anon\\Desktop\\nft_algo1\\" + "\\export" + i + ".png");
+
+                merged1.Dispose();
+                merged2.Dispose();
+                merged3.Dispose();
+
+                
+
+                //JSON serializing 
+                using (StreamWriter sw = new StreamWriter("C:\\Users\\Anon\\Desktop\\nft_algo1\\" + "\\export" + i + ".txt"))
+                {
+
+                    sw.WriteLine("{");
+                    sw.WriteLine("\"attributes\": [{");
+
+                    //layer_one
+                    if (index1 == 0)
+                    {
+                        sw.WriteLine("\"trait_type\": \"Background\", \"value\": \"Space 1\"");
+                    }
+                    else if (index1 == 1)
+                    {
+                        sw.WriteLine("\"trait_type\": \"Background\", \"value\": \"Space 2\"");
+                    }
+                    else if (index1 == 2)
+                    {
+                        sw.WriteLine("\"trait_type\": \"Background\", \"value\": \"Space 3\"");
+                    }
+                    else if (index1 == 3)
+                    {
+                        sw.WriteLine("\"trait_type\": \"Background\", \"value\": \"Space 4\"");
+                    }
+
+                    sw.WriteLine("}, {");
+
+                    //layer_two
+                    if (index2 == 0)
+                    {
+                        sw.WriteLine("\"trait_type\": \"Head\", \"value\": \"Head 1\"");
+                    }
+                    else if (index2 == 1)
+                    {
+                        sw.WriteLine("\"trait_type\": \"Head\", \"value\": \"Head 2\"");
+                    }
+                    else if (index2 == 2)
+                    {
+                        sw.WriteLine("\"trait_type\": \"Head\", \"value\": \"Head 3\"");
+                    }
+                    else if (index2 == 3)
+                    {
+                        sw.WriteLine("\"trait_type\": \"Head\", \"value\": \"Head 4\"");
+                    }
+
+                    sw.WriteLine("}, {");
+
+                    //layer_3
+                    if (index3 == 0)
+                    {
+                        sw.WriteLine("\"trait_type\": \"Smile\", \"value\": \"Smile 1\"");
+                    }
+                    else if (index3 == 1)
+                    {
+                        sw.WriteLine("\"trait_type\": \"Smile\", \"value\": \"Smile 2\"");
+                    }
+                    else if (index3 == 2)
+                    {
+                        sw.WriteLine("\"trait_type\": \"Smile\", \"value\": \"Smile 3\"");
+                    }
+                    else if (index3 == 3)
+                    {
+                        sw.WriteLine("\"trait_type\": \"Smile\", \"value\": \"Smile 4\"");
+                    }
+
+                    sw.WriteLine("}, {");
+
+                    //invisible trait
+                    sw.WriteLine("\"trait_type\": \"Invisible\", \"value\": \"Invisible Trate\"");
+
+                    sw.WriteLine("}");
+                    sw.WriteLine("]");
+                    sw.WriteLine("}");
+
+
+
+                }
+
+                i++;
+
+
+
+            }
+
+        }
+    }
+}
+
